2c2aa16fbbeb15f8956132f0c42d2937ff79df26
YeoJeongHun <aofhd0003@naver.com>
2022-04-06 23:21:17 +0900

    fetch axios 테스트 완료

--- src/components/article/ArticleList.js
+++ src/components/article/ArticleList.js
@@ -8,6 +8,9 @@ import ArticleListTable from './ArticleListTable';
 import ArticleWrite from './ArticleWrite';
 import ArticleDetail from './ArticleDetail';
 import ArticleUpdate from './ArticleUpdate';
+import fetch from 'dva/fetch';
+
+const axios = require('axios');
 
 function showPage (articlePage) {
     switch(articlePage) {
@@ -34,12 +37,29 @@ export default function ArticleList () {
         return dispatch(goToListArticle());
     }
 
+    const fetchTest = () => {
+        fetch('https://jsonplaceholder.typicode.com/todos/1').then((res) => {
+            console.log('res : ', res);
+        })
+        axios.get('https://jsonplaceholder.typicode.com/todos/1')
+        .then(function (response) {
+            // handle success
+            console.log(response);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        console.log('결과 : ');
+    }
+
     return (
         <section>
             <h1>게시글</h1>
             <div>
                 <Button onClick={() => goToArticleWritePage()}>글쓰기 페이지</Button>
                 <Button onClick={() => goToArticleListPage()}>게시글 목록 보기</Button>
+                <Button onClick={() => fetchTest()}>fetch테스트 버튼</Button>
             </div>
             {articlePage === 'list'}
             {showPage(articlePage)}
