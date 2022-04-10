2a892921fa2c9709ddc0e10b719af36606f4b11a
YeoJeongHun <aofhd0003@naver.com>
2022-04-10 21:17:53 +0900

    ArticleList 다시 커밋

---
+++ src/components/article/ArticleList.js
@@ -0,0 +1,60 @@
+import { useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Button } from 'antd';
+import { goToWriteArticle, goToListArticle, goToUpdateArticle } from '../../store/modules/article';
+import ArticleListTable from './ArticleListTable';
+import ArticleWrite from './ArticleWrite';
+import ArticleDetail from './ArticleDetail';
+import ArticleUpdate from './ArticleUpdate';
+import fetch from 'dva/fetch';
+const axios = require('axios');
+function showPage (articlePage) {
+    switch(articlePage) {
+        case 'list':
+            return <ArticleListTable/>;
+        case 'write':
+            return <ArticleWrite/>;
+        case 'detail':
+            return <ArticleDetail/>;
+        case 'update':
+            return <ArticleUpdate/>;
+    }
+}
+export default function ArticleList () {
+    const articlePage = useSelector((state) => state.article.articlePage);
+    const dispatch = useDispatch();
+    const goToArticleWritePage = () => {
+        return dispatch(goToWriteArticle());
+    }
+    const goToArticleListPage = () => {
+        return dispatch(goToListArticle());
+    }
+    const fetchTest = () => {
+        fetch('https://jsonplaceholder.typicode.com/todos/1').then((res) => {
+            console.log('res : ', res);
+        })
+        axios.get('https://jsonplaceholder.typicode.com/todos/1')
+        axios.get('/api')
+        .then(function (response) {
+            // handle success
+            console.log(response);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        console.log('결과 : ');
+    }
+    return (
+        <section>
+            <h1>게시글</h1>
+            <div>
+                <Button onClick={() => goToArticleWritePage()}>글쓰기 페이지</Button>
+                <Button onClick={() => goToArticleListPage()}>게시글 목록 보기</Button>
+                <Button onClick={() => fetchTest()}>fetch테스트 버튼</Button>
+            </div>
+            {articlePage === 'list'}
+            {showPage(articlePage)}
+        </section>
+    )
+}
